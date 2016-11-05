2a3e50a6e729f4f6c93bdafe6301d7624c04c157
mrzhqiang <qiang.zhang@dang5.com>
2016-11-06 04:16:45 +0800

    删除本地对Logger的分析，修改为对Logger的引用； 添加代码头部的LICENSE声明。

--- app/src/main/java/cn/qiang/zhang/customlogger/SampleLoggerActivity.java
+++ app/src/main/java/cn/qiang/zhang/customlogger/SampleLoggerActivity.java
@@ -1,10 +1,29 @@
+/*******************************************************************************
+ *
+ *    Copyright 2016 mrZQ
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ ******************************************************************************/
+
 package cn.qiang.zhang.customlogger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.orhanobut.logger.Logger;
 
 import cn.qiang.zhang.logger.Log;
-import cn.qiang.zhang.logger.logger.Logger;
 
 /**
  * <h3>
--- library/src/main/java/cn/qiang/zhang/logger/Log.java
+++ library/src/main/java/cn/qiang/zhang/logger/Log.java
@@ -1,10 +1,28 @@
+/*******************************************************************************
+ *
+ *    Copyright 2016 mrZQ
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ ******************************************************************************/
+
 package cn.qiang.zhang.logger;
 
 import android.text.TextUtils;
 
-import java.util.Locale;
+import com.orhanobut.logger.Logger;
 
-import cn.qiang.zhang.logger.logger.Logger;
+import java.util.Locale;
 
 /**
  * <h3>
